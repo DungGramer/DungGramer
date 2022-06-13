6258cbbaa6061243656d18ae7ee380d84ee06450
dungnc <dungnc@coccoc.com>
2022-06-13 14:53:06 +0700

    Add linting and format code

--- src/components/download/index.jsx
+++ src/components/download/index.jsx
@@ -1,17 +1,19 @@
-
-export const Download = () => {
-  return (
-    <div class="download">
-    <ul class="download__wrapper">
-      <li
-        ><a
-          class="pdf"
+const Download = () => (
+  <div className="download">
+    <ul className="download__wrapper">
+      <li>
+        <a
+          className="pdf"
           href="//raw.githubusercontent.com/DungGramer/DungGramer/master/NguyenCongDung-resume.pdf"
-          >Download (.pdf)</a
-        ></li
-      >
-      <li><a class="print">Print</a></li>
+        >
+          Download (.pdf)
+        </a>
+      </li>
+      <li>
+        <p className="print">Print</p>
+      </li>
     </ul>
   </div>
-  )
-}
+);
+
+export default Download;
--- src/components/experience/index.jsx
+++ src/components/experience/index.jsx
@@ -1,53 +1,56 @@
-export const Experience = () => {
-  return (
-			<section class="d-flex wrapper">
-      <aside class="header__container">
-        <h2 class="heading">Experience</h2>
-      </aside>
+const Experience = () => (
+  <section className="d-flex wrapper">
+    <aside className="header__container">
+      <h2 className="heading">Experience</h2>
+    </aside>
 
-      <section class="content__container">
-        <article class="info">
-          <header>
-            <h3>Front-end, <span>TTC Solution - Unitel Project</span></h3>
-            <p class="sub-header">December 2020 - February 2021</p>
-          </header>
-          <main>
-            <ul>
-              <li>Designed dynamic pages using Angular</li>
-              <li>Expand feature, refine code</li>
-              <li>
-                Responsive web design, UI using Flex Layout, CSS media query
-              </li>
-              <li>
-                Produced stunning visual elements of web applications by
-                translating UI/UX design wireframes into code while producing
-                high quality, reusable markup using HTML5 and CSS3
-              </li>
-            </ul>
-          </main>
-        </article>
+    <section className="content__container">
+      <article className="info">
+        <header>
+          <h3>
+            Front-end,
+            <span>TTC Solution - Unitel Project</span>
+          </h3>
+          <p className="sub-header">December 2020 - February 2021</p>
+        </header>
+        <main>
+          <ul>
+            <li>Designed dynamic pages using Angular</li>
+            <li>Expand feature, refine code</li>
+            <li>
+              Responsive web design, UI using Flex Layout, CSS media query
+            </li>
+            <li>
+              Produced stunning visual elements of web applications by
+              translating UI/UX design wireframes into code while producing high
+              quality, reusable markup using HTML5 and CSS3
+            </li>
+          </ul>
+        </main>
+      </article>
 
-        <article class="info">
-          <header>
-            <h3>
-              IT Support Technician,
-              <span>Hoang Bach Technology CO., LTD</span>
-            </h3>
-            <p>August 2017 - January 2018</p>
-          </header>
-          <main>
-            <ul>
-              <li>
-                Installing and configuring computer hardware, software,
-                systems, networks, printers and scanners
-              </li>
-              <li>Repairing and replacing equipment as necessary</li>
-              <li>Testing new technology</li>
-              <li>Sales, product consultation</li>
-            </ul>
-          </main>
-        </article>
-      </section>
+      <article className="info">
+        <header>
+          <h3>
+            IT Support Technician,
+            <span>Hoang Bach Technology CO., LTD</span>
+          </h3>
+          <p>August 2017 - January 2018</p>
+        </header>
+        <main>
+          <ul>
+            <li>
+              Installing and configuring computer hardware, software, systems,
+              networks, printers and scanners
+            </li>
+            <li>Repairing and replacing equipment as necessary</li>
+            <li>Testing new technology</li>
+            <li>Sales, product consultation</li>
+          </ul>
+        </main>
+      </article>
     </section>
-  )
-}
+  </section>
+);
+
+export default Experience;
